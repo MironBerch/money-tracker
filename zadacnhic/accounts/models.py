ee45c0433a669a401f349909ccbca104db3186b4
MironBerch <mironberchetov@gmail.com>
2023-08-03 18:59:46 +0300

    Create Profile model
    Create profile model for custom user.
    And add profile model to admin panel.

--- zadacnhic/accounts/models.py
+++ zadacnhic/accounts/models.py
@@ -9,30 +9,30 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom `User` model."""
 
     email = models.EmailField(
-        _('email address'),
+        verbose_name=_('email address'),
         max_length=100,
         unique=True,
     )
 
     first_name = models.CharField(
-        _('first name'),
+        verbose_name=_('first name'),
         max_length=40,
     )
     last_name = models.CharField(
-        _('last name'),
+        verbose_name=_('last name'),
         max_length=40,
     )
 
     is_active = models.BooleanField(
-        _('active'),
+        verbose_name=_('active'),
         default=True,
     )
     is_staff = models.BooleanField(
-        _('staff'),
+        verbose_name=_('staff'),
         default=False,
     )
     is_superuser = models.BooleanField(
-        _('superuser'),
+        verbose_name=_('superuser'),
         default=False,
     )
 
@@ -50,4 +50,57 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     @property
     def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
+
+
+def get_profile_image_upload_path(instance: 'Profile', filename: str) -> str:
+    return f'upload/users/{instance.user.email}/profile/{filename}'
+
+
+def get_default_profile_image() -> str:
+    return 'default/profile/default_profile_image.png'
+
+
+class ProfileGenderChoices(models.TextChoices):
+    MALE = 'M', 'Мальчик'
+    FEMALE = 'F', 'Девочка'
+    OTHER = 'O', ''
+
+
+class Profile(models.Model):
+    """Profile for `User`."""
+
+    profile_image = models.ImageField(
+        verbose_name=_('profile image'),
+        blank=True,
+        null=True,
+        upload_to=get_profile_image_upload_path,
+        default=get_default_profile_image,
+    )
+    date_of_birth = models.DateField(
+        verbose_name=_('date of birth'),
+        blank=True,
+        null=True,
+    )
+    gender = models.CharField(
+        verbose_name=_('gender'),
+        blank=True,
+        max_length=2,
+        choices=ProfileGenderChoices.choices,
+    )
+    description = models.TextField(
+        verbose_name=_('description'),
+        blank=True,
+    )
+    user: User = models.OneToOneField(
+        User,
+        related_name=_('profile'),
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
+
+    def __str__(self):
+        return f'Profile for {self.user}'
