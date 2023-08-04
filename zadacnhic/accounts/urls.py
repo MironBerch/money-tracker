f1ba5a91fa9e16660446ef000b5846b558a277b0
MironBerch <mironberchetov@gmail.com>
2023-08-04 16:53:18 +0300

    Add accounts password reset urls

--- zadacnhic/accounts/urls.py
+++ zadacnhic/accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from accounts.views import SignInView, SignOutView, SignUpView
+from accounts.views import (
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+    SignInView,
+    SignOutView,
+    SignUpView,
+)
 
 urlpatterns = [
     path(
@@ -17,5 +25,27 @@ urlpatterns = [
         route='signout/',
         view=SignOutView.as_view(),
         name='signout',
+    ),
+
+    # password reset urls
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(),
+        name='password_reset',
+    ),
+    path(
+        route='password_reset/done/',
+        view=PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        route='reset/<uidb64>/<token>/',
+        view=PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        route='reset/done/',
+        view=PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
     ),
 ]
