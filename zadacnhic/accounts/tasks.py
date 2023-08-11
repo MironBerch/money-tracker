cb6093bc631846c33e25aa9d3adc8f192b15bfee
MironBerch <mironberchetov@gmail.com>
2023-08-11 17:52:05 +0300

    Create send_password_reset_link celery task

---
+++ zadacnhic/accounts/tasks.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from celery import shared_task
+
+from accounts.services import get_user_by_pk, send_password_reset_email
+
+
+@shared_task
+def send_password_reset_link(
+        subject_template_name: str,
+        email_template_name: str,
+        context: dict,
+        from_email: str,
+        to_email: str,
+        html_email_template_name: Optional[str] = None,
+) -> None:
+    """Send password reset email celery task."""
+    context['user'] = get_user_by_pk(pk=context['user'])
+    send_password_reset_email(
+        subject_template_name=subject_template_name,
+        email_template_name=email_template_name,
+        context=context,
+        from_email=from_email,
+        to_email=to_email,
+        html_email_template_name=html_email_template_name,
+    )
