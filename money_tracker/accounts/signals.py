507dd5d322b2bb02c5f59a439710ee0c25006f44
MironBerch <mironberchetov@gmail.com>
2023-08-13 20:38:49 +0300

    Create create_user_settings signal

--- money_tracker/accounts/signals.py
+++ money_tracker/accounts/signals.py
@@ -1,10 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from accounts.models import Profile, User
+from accounts.models import Profile, Settings, User
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def create_user_settings(sender, instance, created, **kwargs):
+    if created:
+        Settings.objects.create(user=instance)
