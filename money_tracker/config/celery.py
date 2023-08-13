866cf99763f85873fb2d576510680630f4e1b0f0
MironBerch <mironberchetov@gmail.com>
2023-08-13 18:41:07 +0300

    Create Category model
    Create Category model and make migrations.
    Change celery app name.

--- money_tracker/config/celery.py
+++ money_tracker/config/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('zadacnhic')
+app = Celery('money_tracker')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
