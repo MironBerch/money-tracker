505187cbf97345bc2557e1428fe062327cf3400f
MironBerch <mironberchetov@gmail.com>
2023-08-15 19:40:06 +0300

    Create CategoryCreateView post method

--- money_tracker/categories/views.py
+++ money_tracker/categories/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.utils.text import slugify
 from django.views.generic import View
 from django.views.generic.base import TemplateResponseMixin
 
@@ -22,3 +24,12 @@ class CategoryCreateView(
                 'form': self.form_class(),
             },
         )
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        form = CategoryForm(request.POST or None)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.slug = slugify(category.name)
+            category.user = request.user
+            category.save()
+            return redirect('expenses_list')
