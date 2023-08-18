b6e583b36acfd58c239d0a3b2f6d936af8f77728
MironBerch <mironberchetov@gmail.com>
2023-08-18 15:08:06 +0300

    Create get_expense_by_id and get_user_expense_by_id services

--- money_tracker/expenses/services.py
+++ money_tracker/expenses/services.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 
 from accounts.models import User
 from categories.models import Category
@@ -13,3 +14,13 @@ def get_user_expenses(user: User) -> QuerySet[Expense]:
 def get_expenses_by_category(category: Category) -> QuerySet[Expense]:
     """Get `Expense`'s by category."""
     return Expense.objects.filter(category=category)
+
+
+def get_expense_by_id(id: int) -> Expense:
+    """Get `Expense` by id."""
+    return get_object_or_404(Expense, id=id)
+
+
+def get_user_expense_by_id(user: User, id: int) -> Expense:
+    """Get `Expense` by id."""
+    return get_object_or_404(Expense, user=user, id=id)
