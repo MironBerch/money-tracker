8165898882a91b22ea6e02fb7a1b0c38c24291b4
MironBerch <mironberchetov@gmail.com>
2023-08-20 17:40:39 +0300

    Create Budget model and create migrations

---
+++ money_tracker/budgets/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from accounts.models import User
+
+
+class Budget(models.Model):
+    """Budget for `User`."""
+
+    budget = models.FloatField(
+        verbose_name=_('overall budget'),
+    )
+
+    user: User = models.OneToOneField(
+        User,
+        related_name=_('budget'),
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = _('budget')
+        verbose_name_plural = _('budgets')
+
+    def __str__(self):
+        return f'{self.budget}'
