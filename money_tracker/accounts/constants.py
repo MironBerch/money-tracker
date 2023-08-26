8c2601c68208306f7a548074aa1c0f705252c62e
MironBerch <mironberchetov@gmail.com>
2023-08-27 01:15:14 +0300

    Add accounts constants

---
+++ money_tracker/accounts/constants.py
@@ -0,0 +1,11 @@
+# Indicates that confirmation email has been sent successfully
+EMAIL_SENT_SUCCESSFULLY_RESPONSE_MESSAGE = 'We have sent a confirmation email to your email address'
+
+# Indicates that email has been successfully confirmed
+EMAIL_CONFIRMATION_SUCCESS_RESPONSE_MESSAGE = 'You have successfully confirmed your email'
+
+# Indicates that email hasn't been confirm
+EMAIL_CONFIRMATION_FAILURE_RESPONSE_MESSAGE = 'There was an error while confirming your email'
+
+# Indicates that profile has been successfully updated
+PROFILE_INFO_EDIT_SUCCESS_RESPONSE_MESSAGE = 'Your profile has been successfully updated'
