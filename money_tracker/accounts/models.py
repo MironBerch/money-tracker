8b1aca98c42d590ae10ccb6b75f0b787fdf7cc17
MironBerch <mironberchetov@gmail.com>
2023-08-30 14:42:58 +0300

    Create TelegramUserVerifyCode model

--- money_tracker/accounts/models.py
+++ money_tracker/accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -147,3 +148,29 @@ class Settings(models.Model):
 
     def __str__(self):
         return f'Settings for {self.user}'
+
+
+class TelegramUserVerifyCode(models.Model):
+    """Code for connecting the `User` with the telegram user."""
+
+    telegram_code = models.CharField(
+        verbose_name=_('4 digits code'),
+        unique=True,
+        blank=True,
+        max_length=4,
+        validators=[
+            MinLengthValidator(4),
+        ],
+    )
+    settings: Settings = models.OneToOneField(
+        Settings,
+        related_name='telegram_verify_code',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = _('telegram code')
+        verbose_name_plural = _('telegram codes')
+
+    def __str__(self):
+        return f'Code for `{self.telegram_code}` for {self.settings.user}'
